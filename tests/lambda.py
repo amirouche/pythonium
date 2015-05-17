4870a509e306b3acde66bb0855dbec2453b20c35
amirouche <amirouche.boubekki@gmail.com>
2015-05-17 15:17:33 +0200

    tests/lambda.py s/"value"/2006 because str + int is not ok in Python

--- tests/lambda.py
+++ tests/lambda.py
@@ -1,4 +1,4 @@
 def func(x, y, arg, kwargs):
     return x + y + arg[0] + arg[1] + kwargs["key"]
 f = lambda x, y, *args, **kwargs: func(x, y, args, kwargs) + 1
-print(f(42, 1337, 11, 100, key="value"))
+print(f(42, 1337, 11, 100, key=2006))
